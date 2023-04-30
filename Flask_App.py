3fbc5bbb2a8c335987a33c89dc9641174638d950
Augusta <tsampiaugusta991@yahoo.com>
2023-05-01 00:29:58 +0200

    .

--- Flask_App.py
+++ Flask_App.py
@@ -10,8 +10,8 @@ api_key=os.environ.get("API_KEY")
 app = FastAPI()
 
 @app.get("/")
-async def read_item(latitude, longitude):
-    url = f"https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={api_key}"
+async def read_item(lat, lon):
+    url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_Key}"
     result = requests.get(url)
     data = result.json()
             
